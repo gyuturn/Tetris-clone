4326764c822b8a442efd0012b10c2a98fac8b758
turn <kksy2423@naver.com>
2021-09-30 03:28:00 +0900

    testirs-3

--- tetris.js
+++ tetris.js
@@ -1,3 +1,5 @@
+import BLOKCS from "./blocks.js";
+
 //DOM
 const playground = document.querySelector(".playground > ul");
 
@@ -11,14 +13,7 @@ let duration = 500;
 let downInterval;
 let tempMovingItem;
 
-const BLOKCS={
-    tree: [
-        [[2,1],[0,1],[1,0],[1,1]],
-        [],
-        [],
-        [],
-    ]
-}
+
 const monvingItem={
      type:"tree",
      direction:0,
@@ -31,6 +26,10 @@ init();
 
 //functions
 function init(){
+    
+    // blockArray.forEach(block=>{
+        
+    // })
     tempMovingItem={...monvingItem};
    
     for( let i=0;i<20;i++){
@@ -53,7 +52,7 @@ function prerpendNewLine(){
 
 }
 
-function renderBlocks(){
+function renderBlocks(moveType=""){
     const { type, direction,top,left}=tempMovingItem; 
     const movingBlocks= document.querySelectorAll(".moving");
      movingBlocks.forEach(moving=>{
@@ -61,14 +60,14 @@ function renderBlocks(){
      })
 
      
-    BLOKCS[type][direction].forEach(block => {
+    BLOKCS[type][direction].some(block => {
         const x = block[0]+left;
         const y= block[1]+top;
         
         
         
         const target = playground.childNodes[y]? playground.childNodes[y].childNodes[0].childNodes[x] :  null;
-        console.log(target);
+        console.log( playground.childNodes[y]);
         const isAvailabe = checkEmpty(target);
         if(isAvailabe){
              target.classList.add(type,"moving"); 
@@ -80,8 +79,9 @@ function renderBlocks(){
                 if(moveType==="top"){
                     seizeBlock();
                 }
-            },0);
-            
+                
+            },0)
+            return true;
         }
        
     })
@@ -90,12 +90,29 @@ function renderBlocks(){
     monvingItem.direction=direction;
 } 
 
-//40:41 seizeBlock부터 만들기 settimeout 함수 왜 넣는거지??
+ 
 function seizeBlock(){
+    const movingBlocks= document.querySelectorAll(".moving");
+    movingBlocks.forEach(moving=>{
+        moving.classList.remove("moving");
+        moving.classList.add("seized");
+    })
+    generateNewBlock();
+}
 
+function generateNewBlock(){
+    const blockArray= Object.entries(BLOKCS);
+    const randomIndex= Math.floor(Math.random()*blockArray.length);
+    
+    monvingItem.type=blockArray[randomIndex][0];
+    monvingItem.top=0;
+    monvingItem.left=3;
+    monvingItem.direction =0;
+    tempMovingItem={...monvingItem};
+    renderBlocks();
 }
 function checkEmpty(target){
-    if(!target){
+    if(!target || target.classList.contains("seized")){
     return false;
     }
     return true;
@@ -103,9 +120,14 @@ function checkEmpty(target){
 
 function moveBlock(moveType,amount){
     tempMovingItem[moveType] +=amount;
-    renderBlocks();
+    renderBlocks(moveType);
 }
 
+function changeDirection(){
+    const direction = tempMovingItem.direction;
+    direction === 3? tempMovingItem.direction = 0 : tempMovingItem.direction+=1; 
+    renderBlocks();
+}
 
 //event handling
 document.addEventListener("keydown", e =>{
@@ -119,7 +141,9 @@ document.addEventListener("keydown", e =>{
         case 40:
             moveBlock("top",1);
             break;
+        case 38:
+            changeDirection();
     }
 
-    console.log(e);
+    
 })
